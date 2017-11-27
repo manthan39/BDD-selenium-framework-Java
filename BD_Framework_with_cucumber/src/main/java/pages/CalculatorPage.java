a1491c49a7f37c9f60f19856448dc26121444807
mvbhatiya <mvbhatiya@CIPL-6PC59.cygnet.com>
2017-11-27 19:32:08 +0530

    Calculator class & UI actions are created

---
+++ BD_Framework_with_cucumber/src/main/java/pages/CalculatorPage.java
@@ -0,0 +1,51 @@
+package pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import locators.CalculatorPageLocator;
+import locators.CalculatorPageLocator.IautoLocator;
+import testBase.testBase;
+
+public class CalculatorPage extends testBase {
+
+	
+	@FindBy(xpath=IautoLocator.FirstInput)
+	 private WebElement firstInput;
+	
+	@FindBy(xpath=IautoLocator.SecondInput)
+	 private WebElement SecondInput;
+	
+	@FindBy(xpath=IautoLocator.SelectSymbol)
+	 private WebElement SelectSymbol;
+	
+	@FindBy(css=IautoLocator.GoBtn)
+	 private WebElement GoBtn;
+	
+	public void EnterFirstInput(String input){
+		firstInput.sendKeys(input);
+	}
+	
+	public void EnterSecondInput(String input){
+		firstInput.sendKeys(input);
+	}
+	
+	public void SelectFromDropDown(String symbol){
+		Select sc = new Select(SelectSymbol);
+		sc.selectByVisibleText(symbol);
+	}
+	
+	public void clickOnGoBtn(){
+		GoBtn.click();
+	}
+	
+	public CalculatorPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+}
